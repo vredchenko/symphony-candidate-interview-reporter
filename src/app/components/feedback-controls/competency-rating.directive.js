30f12a12f382c33b85aa1076b388079fb072ee73
Val Redchenko <valeriy.redchenko@symphony-solutions.eu>
2015-04-25 21:23:03 +0300

    Added an example directive

---
+++ src/app/components/feedback-controls/competency-rating.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+//ngInject
+function CompetencyRating($http) {
+  return {
+    restrict: 'AE',
+    template: '<div> Example </div>'
+  };
+}
+export default CompetencyRating;
--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,7 @@ import LandingCtrl from './landing/landing.controller';
 import InterviewCtrl from './interview/interview.controller';
 import ReportCtrl from './report/report.controller';
 import MainCtrl from './main/main.controller';
+import CompetencyRating from './components/feedback-controls/competency-rating.directive';
 
 angular.module('interviewReport', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'mgcrea.ngStrap'])
   .controller('LandingCtrl', LandingCtrl)
@@ -12,6 +13,7 @@ angular.module('interviewReport', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanit
   .controller('ReportCtrl', ReportCtrl)
   .controller('NavbarCtrl', NavbarCtrl)
   .controller('MainCtrl', MainCtrl)
+  .directive('competencyRating', CompetencyRating)
   
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
